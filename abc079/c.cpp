361a168c5681aee75807422f90c96cd1496ee701
iwakiriK <balssearch0113@gmail.com>
2018-04-12 09:51:58 +0900

    Add abc079 c

---
+++ abc079/c.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+    int a = s[0] - '0', b = s[1] - '0', c = s[2] - '0', d = s[3] - '0';
+    if (a - b - c - d == 7) printf("%d-%d-%d-%d=7\n", a, b, c, d);
+    else if (a - b - c + d == 7) printf("%d-%d-%d+%d=7\n", a, b, c, d);
+    else if (a - b + c - d == 7) printf("%d-%d+%d-%d=7\n", a, b, c, d);
+    else if (a - b + c + d == 7) printf("%d-%d+%d+%d=7\n", a, b, c, d);
+    else if (a + b - c - d == 7) printf("%d+%d-%d-%d=7\n", a, b, c, d);
+    else if (a + b - c + d == 7) printf("%d+%d-%d+%d=7\n", a, b, c, d);
+    else if (a + b + c - d == 7) printf("%d+%d+%d-%d=7\n", a, b, c, d);
+    else if (a + b + c + d == 7) printf("%d+%d+%d+%d=7\n", a, b, c, d);
+
+    return 0;
+}
