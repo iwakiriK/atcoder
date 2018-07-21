942653436c461910254e5b5a972a331802d7a37c
iwakiriK <balssearch0113@gmail.com>
2018-07-22 00:05:18 +0900

    Updae abc103 d

--- abc103/d.cpp
+++ abc103/d.cpp
@@ -1,20 +1,19 @@
 #include <bits/stdc++.h>
-#define rep(i,n) for(int i=0;i<(n);++i)
 using namespace std;
 
 int main() {
-    int n, m, num, ans = 0;
+    int n, m, ans = 0;
     cin >> n >> m;
     vector<pair<int, int> >V(m);
-    rep(i, m) {
-        cin >> V[i].second >> V[i].first;
+    for (auto &x: V) {
+        cin >> x.second >> x.first;
     }
     sort(V.begin(), V.end());
     int b = -1;
-    rep(i, m) {
-        if (b <= V[i].second) {
+    for (auto x: V) {
+        if (b <= x.second) {
             ans++;
-            b = V[i].first;
+            b = x.first;
         }
     }
     cout << ans << endl;
