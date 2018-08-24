dee12bde6bfad8ff53e266a0a82eeb4a9fa60c17
iwakiriK <balssearch0113@gmail.com>
2018-08-24 20:44:14 +0900

    Update template(change from shell to makefile)

--- workspace/runb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -o out b.cpp && ./out < in1.txt && echo "********************" && ./out < in2.txt && echo "********************" && ./out < in3.txt
--- workspace/runc.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-g++ -o out c.cpp && ./out < in1.txt && echo "********************" && ./out < in2.txt && echo "********************" && ./out < in3.txt
