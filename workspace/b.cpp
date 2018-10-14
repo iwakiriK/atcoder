4046cefe5b224d941607dd90e96dcb8bbb04a7b5
iwakiriK <balssearch0113@gmail.com>
2018-10-15 00:25:28 +0900

    Add REP

--- workspace/b.cpp
+++ workspace/b.cpp
@@ -1,9 +1,10 @@
 #include<bits/stdc++.h>
-#define rep(i,a,b) for(int i=(a);i<(b);i++)
+#define REP(i,a,b) for(int i=(a);i<(b);i++)
+#define rep(i,n) REP(i,0,(n))
 typedef long long ll;
 using namespace std;
 
 int main() {
-    
+
     return 0;
 }
