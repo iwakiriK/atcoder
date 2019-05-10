9f5ae0d3474327c62f397f8d6bb97b26375a68b3
iwakiriK <balssearch0113@gmail.com>
2019-05-10 17:31:09 +0900

    Add abc047 b

--- workspace/b.cpp
+++ workspace/b.cpp
@@ -5,6 +5,18 @@ typedef long long ll;
 using namespace std;
 
 int main() {
+    int w, h, n, x, y, a;
+    cin >> w >> h >> n;
     
-    return 0;
+    int minX = 0, maxX = w, minY = 0, maxY = h;
+    rep(i, n) {
+        cin >> x >> y >> a;
+        if (a == 1) minX = max(minX, x);
+        if (a == 2) maxX = min(maxX, x);
+        if (a == 3) minY = max(minY, y);
+        if (a == 4) maxY = min(maxY, y);
+    }
+    int width  = max(maxX - minX, 0);
+    int height = max(maxY - minY, 0);
+    cout << width * height << endl;
 }
