0dbdeefe9e6389737f8365841f2acd2ac43778af
iwakiriK <balssearch0113@gmail.com>
2019-05-10 17:32:25 +0900

    retrun

--- workspace/b.cpp
+++ workspace/b.cpp
@@ -5,18 +5,6 @@ typedef long long ll;
 using namespace std;
 
 int main() {
-    int w, h, n, x, y, a;
-    cin >> w >> h >> n;
     
-    int minX = 0, maxX = w, minY = 0, maxY = h;
-    rep(i, n) {
-        cin >> x >> y >> a;
-        if (a == 1) minX = max(minX, x);
-        if (a == 2) maxX = min(maxX, x);
-        if (a == 3) minY = max(minY, y);
-        if (a == 4) maxY = min(maxY, y);
-    }
-    int width  = max(maxX - minX, 0);
-    int height = max(maxY - minY, 0);
-    cout << width * height << endl;
+    return 0;
 }
