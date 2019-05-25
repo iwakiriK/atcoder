2c594b9b042bd8b45f73188de1b4250ecf6044fd
iwakiriK <balssearch0113@gmail.com>
2019-05-25 13:53:12 +0900

    Add e f

---
+++ workspace/f.cpp
@@ -0,0 +1,10 @@
+#include<bits/stdc++.h>
+#define REP(i,a,b) for(int i=(a);i<(b);i++)
+#define rep(i,n) REP(i,0,(n))
+typedef long long ll;
+using namespace std;
+
+int main() {
+    
+    return 0;
+}
