e68f171be60c316e5bc5252593d1b303a5b447db
iwakiriK <balssearch0113@gmail.com>
2019-05-25 15:41:11 +0900

    Add chokuadi_S002 e

---
+++ chokudai_S002/e.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+typedef long long ll;
+using namespace std;
+
+ll n, a, b, ans;
+
+int main() {
+    cin >> n;
+    rep(i, n) {
+        cin >> a >> b;
+        ans += min(a / 2, b);
+    }
+    cout << ans << endl;
+}
