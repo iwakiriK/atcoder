080b17027308ffa41e970e579f1dd216b05ff25a
iwakiriK <balssearch0113@gmail.com>
2019-06-28 16:01:39 +0900

    Add abc093 b

---
+++ abc093/b.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int A, B, K;
+    cin >> A >> B >> K;
+    for (int i = A; i <= B; i++) {
+        if (i < A + K || B - K < i) {
+            cout << i << endl;
+        }
+    }
+}
