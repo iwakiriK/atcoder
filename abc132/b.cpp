523ac369447209e3f8e40453d18c31ab84418eed
iwakiriK <balssearch0113@gmail.com>
2019-07-11 10:43:31 +0900

    Add abc132 b

---
+++ abc132/b.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#define rep(i,n) for(int i=0;i<n;++i)
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    int p[n], ans = 0;
+    rep(i, n) cin >> p[i];
+    rep(i, n-2) {
+        if (p[i] < p[i+1] && p[i+1] < p[i+2]) ans++;
+        if (p[i] > p[i+1] && p[i+1] > p[i+2]) ans++;
+    }
+    cout << ans << endl;
+}
