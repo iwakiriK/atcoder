d9a9006e9d0d96f570ad20a94d1084ab2ea0ee9b
iwakiriK <balssearch0113@gmail.com>
2019-12-22 22:58:20 +0900

    Add abc148 b

---
+++ abc148/b.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int N;
+    string S, T;
+    cin >> N >> S >> T;
+    for (int i = 0; i < N; i++) {
+        cout << S[i] << T[i];
+    }
+    cout << endl;
+}
