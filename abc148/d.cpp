89dbdd897e46b6f419bb7d0aaaa92c9f2d390b3c
iwakiriK <balssearch0113@gmail.com>
2019-12-22 22:58:28 +0900

    Add abc148 d

---
+++ abc148/d.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int N, a, ans, K;
+
+int main() {
+    cin >> N;
+    for (int i = 0; i < N; i++) {
+        cin >> a;
+        if (a == K + 1) K++;
+        else ans++;
+    }
+    
+    cout << (K ? ans : -1) << endl;
+}
