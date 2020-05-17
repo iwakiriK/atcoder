86f1bc87299ec6f770f43c73a9f2571c59e68e27
iwakiriK <balssearch0113@gmail.com>
2020-05-17 23:07:45 +0900

    Add abc168 b

---
+++ abc168/b.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+    int K;
+    string S;
+    cin >> K >> S;
+    if (S.size() > K) {
+        S = S.substr(0, K) + "...";
+    }
+    cout << S << endl;
+}
